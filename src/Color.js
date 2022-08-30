4426472f71e9e2b96b8ca8c56e56b091028dc316
Kumar Chandresh <kumarchandreshsingh@gmail.com>
2022-08-30 13:45:52 +0530

    Implement color copy button

--- src/Color.js
+++ src/Color.js
@@ -2,11 +2,38 @@ import { Component } from "react";
 import styles from "./Color.module.css";
 
 export default class Color extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMouseOver: false };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.cssClasses = [styles.root];
+  }
+  handleClick() {
+    navigator.clipboard.writeText(this.props.value);
+  }
+  handleMouseEnter() {
+    this.setState({ isMouseOver: true });
+  }
+  handleMouseLeave() {
+    this.setState({ isMouseOver: false });
+  }
   render() {
+    
     return (<figure
-      className={styles.root}
-      style={{ backgroundColor: this.props.value }}>
+      className={this.cssClasses.concat([this.state.isMouseOver ? styles.btnGrow : styles.btnShrink]).join(" ")}
+      style={{ backgroundColor: this.props.value }}
+      onClick={this.handleClick}
+      onMouseEnter={this.handleMouseEnter}
+      onMouseLeave={this.handleMouseLeave}
+    >
       <div className={styles.wrapper}>
+        {
+          this.state.isMouseOver
+            ? <input type="submit" value="copy" className={styles.copyBtn}/>
+            : null
+        }
         {
           this.props.name
             ? <figcaption className={styles.caption}>
